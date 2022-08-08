185e81de68e84849f6b596a80d5eee5de7f57ca3
Julia Dannerth <jdannerth@gmail.com>
2022-08-08 09:06:15 -0400

    Solved lab

--- src/index.js
+++ src/index.js
@@ -26,8 +26,45 @@ const cards = [
 ];
 
 const memoryGame = new MemoryGame(cards);
+memoryGame.shuffleCards();
 
-window.addEventListener('load', (event) => {
+function toggleCards(element, classesArray){
+  //console.log({element});
+  classesArray.forEach((className) => element.classList.toggle(className));
+}
+
+// ================ START KEYPRESS EVENTS ===================
+// key events: 'keypress' is used to register number and letter keys when press, 'keydown' & 'keyup' are used to register anu key on the keyboard that is pressed
+
+ window.addEventListener('keydown', (event) => {
+   const h1Tag = document.getElementsByTagName('h1')[0];
+    console.log({event: event.code, theH1: h1Tag.innerHTML});
+
+    switch(event.code){
+      case 'ArrowUp':
+        case 'KeyW':
+          h1Tag.innerHTML = 'Superhero Memory Game'
+          break;
+          case 'ArrowRight':
+            case 'KeyD':
+              h1Tag.innerHTML = 'Superhero Memory Game'
+              break;
+              case 'ArrowLeft':
+                case 'KeyA':
+                  h1Tag.innerHTML = 'Superhero Memory Game'
+                  break;
+                  case 'ArrowDown':
+                    case 'KeyS':
+                      h1Tag.innerHTML = 'Superhero Memory Game'
+                      break;
+                      default:
+                        h1Tag.innerHTML = 'Superhero Memory Game'
+    }
+  })
+
+// ================ END KEYPRESS EVENTS ===================
+
+  window.addEventListener('load', (event) => {
   let html = '';
   memoryGame.cards.forEach((pic) => {
     html += `
@@ -45,7 +82,65 @@ window.addEventListener('load', (event) => {
   document.querySelectorAll('.card').forEach((card) => {
     card.addEventListener('click', () => {
       // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+      // console.log(`Card clicked: ${card}`);
+      //console.log({card})
+
+      //these are up here because he declare the variables at the top
+      const clicked = document.getElementById("pairs-clicked")
+      const guessed = document.getElementById("pairs-guessed")
+
+      console.log({clicked: clicked.innerHTML, guessed: guessed.innerHTML});
+
+
+      if(!card.classList.contains('blocked')){
+      toggleCards(card.children[0], ['front', 'back']);
+      toggleCards(card.children[1], ['front', 'back']);
+
+      card.classList.add('blocked')
+
+
+      memoryGame.pickedCards.push(card);
+      }
+
+      if(memoryGame.pickedCards.length === 2){
+        const firstPicked = memoryGame.pickedCards[0];
+        const secondPicked = memoryGame.pickedCards[1];
+
+        // console.log((firstPicked, secondPicked));
+
+        const cardName1 = firstPicked.getAttribute('data-card-name');
+        const cardName2 = secondPicked.getAttribute('data-card-name');
+
+        // console.log({cardName1, cardName2});
+
+        if(memoryGame.checkIfPair(cardName1, cardName2)){
+          firstPicked.children[1].classList.add('blocked')
+          secondPicked.children[1].classList.add('blocked')
+
+          memoryGame.pickedCards = [];
+
+          guessed.innerHTML = memoryGame.pairsGuessed;
+        } else {
+          setTimeout(() =>{
+            toggleCards(firstPicked.children[0], ['front','back']);
+            toggleCards(firstPicked.children[1], ['front','back']);
+            toggleCards(secondPicked.children[0], ['front','back']);
+            toggleCards(secondPicked.children[1], ['front','back']);
+            firstPicked.classList.remove('blocked');
+            secondPicked.classList.remove('blocked');
+          }, 1000);
+        }
+
+        memoryGame.pickedCards = [];
+      }
+
+      clicked.innerHTML = memoryGame.pairsClicked;
+
+      if(memoryGame.checkIfFinished()){
+        setTimeout(() => {
+        alert('You Have Won This Challenge!')
+      },10)
+    }
     });
   });
 });
--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,75 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
   shuffleCards() {
-    // ... write your code here
+    if(!this.cards){
+      return undefined
+    }
+      for (let i = this.cards.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * i);
+      let temp = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = temp;
+  }   
+    return this.cards;
   }
 
+  // shuffleCards() {
+  //   if(!this.cards){
+  //     return undefined
+  //   }
+
+  // let arrLength = this.cards.length;
+
+  // while (arrLength > 0){
+        //**get a random number from 0 to the length of the array of cards -1 or whatever number arrLength is currently equal to -1**
+  // let index = Math.floor(Math.random() * arrLength);
+
+        //** here we reduce the number of arrLength by 1 so that the last card in the card array does not get changed everytime the loop runs or the card that corresponds to the arrLength in the cards array**
+  // arrLength --;
+
+        // **create a temp variable that store the last element in the array ot the index which corresponds to the cardds [arrLength];**
+  // let tempCard = this.cards[arrLength];
+
+        // **fill the last element in the array of cards with the random card**
+  // this.cards[arrLength]=this.cards[index]
+
+        // **fill the empty space where the random element was with the temp card;**
+  // this.cards[index] = tempCard;
+
+      // ** this is the same thing that is happening on line 37 - 43 above
+  // ** OR: [[this.cards[arrLength], this.cards[index]]] = [[this.cards[index], this.cards[arr.Length]]]
+
+  // }
+  //   return this.cards;
+  // }
+  
   checkIfPair(card1, card2) {
-    // ... write your code here
+    this.pairsClicked++;
+    if (card1 === card2){
+      this.pairsGuessed++;
+      return true
+    } else {
+      return false
+    }  
   }
 
   checkIfFinished() {
-    // ... write your code here
+    if(this.pairsGuessed === this.cards.length / 2){
+    return true;
+    }
+    return false;
+    }
+
+    checkIfLost(){
+      if (this.pairsClicked === this.cards.length){
+        return true;
+      }
+      return false;
+    }
   }
-}
